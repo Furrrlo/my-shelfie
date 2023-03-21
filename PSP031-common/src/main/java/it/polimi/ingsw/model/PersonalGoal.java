95f612904d63dd63f822c21fdec494662bec47c1
Furrrlo <ferloo@protonmail.com>
2023-03-21 19:34:31 +0100

    Add error checking in PersonalGoal constructor

--- PSP031-common/src/main/java/it/polimi/ingsw/model/PersonalGoal.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/model/PersonalGoal.java
@@ -5,6 +5,13 @@ public class PersonalGoal implements PersonalGoalView {
     private final Tile[][] personalGoal;
 
     public PersonalGoal(Tile[][] personalGoal) {
+        if(personalGoal.length != ROWS)
+            throw new IllegalArgumentException("Provided shelf combination has the wrong row size");
+        for(int row = 0; row < personalGoal.length; row++) {
+            if (personalGoal[row].length != COLUMNS)
+                throw new IllegalArgumentException("Provided shelf combination has the wrong column size at row " + row);
+        }
+
         this.personalGoal = personalGoal;
     }
 
