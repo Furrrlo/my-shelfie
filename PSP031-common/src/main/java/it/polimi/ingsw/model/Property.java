405800cf32e6bcfcbcf3788c77029dd89fe43d5f
Furrrlo <ferloo@protonmail.com>
2023-03-22 11:42:09 +0100

    Add Property#update(...) method

--- PSP031-common/src/main/java/it/polimi/ingsw/model/Property.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/model/Property.java
@@ -1,6 +1,12 @@
 package it.polimi.ingsw.model;
 
+import java.util.function.Function;
+
 public interface Property<T> extends Provider<T> {
 
     void set(T val);
+
+    default void update(Function<T, T> updater) {
+        set(updater.apply(get()));
+    }
 }
