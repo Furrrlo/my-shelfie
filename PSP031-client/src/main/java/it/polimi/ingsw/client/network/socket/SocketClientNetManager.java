80b2296b11371aed1314f8b9bd89de219f0f6e70
jackorse <giacomo.orsenigo@gmail.com>
2023-03-25 10:46:32 +0100

    Use CompletableFuture to start gameUpdater asynchronously

--- PSP031-client/src/main/java/it/polimi/ingsw/client/network/socket/SocketClientNetManager.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/network/socket/SocketClientNetManager.java
@@ -3,11 +3,13 @@ package it.polimi.ingsw.client.network.socket;
 import it.polimi.ingsw.client.network.ClientNetManager;
 import it.polimi.ingsw.model.Lobby;
 import it.polimi.ingsw.model.LobbyView;
-import it.polimi.ingsw.socket.packets.*;
+import it.polimi.ingsw.socket.packets.JoinGamePacket;
+import it.polimi.ingsw.socket.packets.LobbyPacket;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.concurrent.CompletableFuture;
 
 public class SocketClientNetManager implements ClientNetManager {
     private final ClientSocketManager socketManager;
@@ -25,7 +27,9 @@ public class SocketClientNetManager implements ClientNetManager {
         Lobby lobby;
         try (var lobbyCtx = socketManager.send(new JoinGamePacket(nick), LobbyPacket.class)) {
             lobby = lobbyCtx.getPacket().lobby();
-            new Thread(new SocketLobbyClientUpdater(lobby, socketManager)).start();
+            CompletableFuture<SocketGameClientUpdater> gamePromise = CompletableFuture
+                    .supplyAsync(new SocketLobbyClientUpdater(lobby, socketManager));
+            gamePromise.thenAcceptAsync(SocketGameClientUpdater::run);
         }
         return lobby;
     }
--- PSP031-client/src/main/java/it/polimi/ingsw/client/network/socket/SocketLobbyClientUpdater.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/network/socket/SocketLobbyClientUpdater.java
@@ -8,12 +8,12 @@ import it.polimi.ingsw.socket.SocketLobbyUpdater;
 import it.polimi.ingsw.socket.packets.CreateGamePacket;
 import it.polimi.ingsw.socket.packets.S2CPacket;
 import it.polimi.ingsw.socket.packets.UpdateJoinedPlayerPacket;
-import it.polimi.ingsw.updater.GameUpdater;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Supplier;
 
-public class SocketLobbyClientUpdater implements SocketLobbyUpdater, Runnable {
+public class SocketLobbyClientUpdater implements SocketLobbyUpdater, Supplier<SocketGameClientUpdater> {
 
     private final Lobby lobby;
     private final ClientSocketManager socketManager;
@@ -24,20 +24,20 @@ public class SocketLobbyClientUpdater implements SocketLobbyUpdater, Runnable {
     }
 
     @Override
-    public void run() {
+    public SocketGameClientUpdater get() {
         do {
             try (var ctx = socketManager.receive(S2CPacket.class)) {
                 final S2CPacket p = ctx.getPacket();
                 if (p instanceof UpdateJoinedPlayerPacket)
                     updateJoinedPlayers(((UpdateJoinedPlayerPacket) p).players());
                 else if (p instanceof CreateGamePacket) {
-                    updateGame(((CreateGamePacket) p).game());
-                    break;
+                    return updateGame(((CreateGamePacket) p).game());
                 }
             } catch (IOException | DisconnectedException e) {
                 throw new RuntimeException(e); //TODO:
             }
         } while (!Thread.interrupted());
+        throw new RuntimeException();
     }
 
     @Override
@@ -46,11 +46,9 @@ public class SocketLobbyClientUpdater implements SocketLobbyUpdater, Runnable {
     }
 
     @Override
-    @SuppressWarnings("NullAway") //TODO: remove this line
-    public GameUpdater updateGame(GameAndController<Game> gameAndController) throws DisconnectedException {
+    public SocketGameClientUpdater updateGame(GameAndController<Game> gameAndController) throws DisconnectedException {
         lobby.game().set(gameAndController);
-        new Thread(new SocketGameClientUpdater(gameAndController.game(), socketManager)).start();
-        return null; //TODO: ?
+        return new SocketGameClientUpdater(gameAndController.game(), socketManager);
     }
 
 }
