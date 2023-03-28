039ce73c1271321d0626f97100067a9d2e10d05a
giovanni <giovannialbertos@hotmail.it>
2023-03-28 19:42:05 +0200

    overrided remaining common goal checks in Type

--- PSP031-common/src/main/java/it/polimi/ingsw/model/Type.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/model/Type.java
@@ -32,8 +32,10 @@ public enum Type implements Serializable {
     },
     ALL_CORNERS{
         @Override
-        public boolean checkCommonGoal(Shelfie shelfie) {
-            return false;
+        public boolean checkCommonGoal(Shelfie shelfie){
+            return  shelfie.tile(0,0).equals(shelfie.tile(0,COLUMNS-1)) &&
+                    shelfie.tile(0,COLUMNS-1).equals(shelfie.tile(ROWS-1,0)) &&
+                    shelfie.tile(ROWS-1,0).equals(shelfie.tile(ROWS-1,COLUMNS-1));
         }
     },
     FOUR_QUADRIPLETS{
@@ -91,9 +93,35 @@ public enum Type implements Serializable {
         }
     },
     TWO_SQUARES{
+        /**
+         *
+         * @param shelfie
+         * @return the number of existing squares of given color in a given shelfie
+         */
+        public int numSquares(Shelfie shelfie){
+            int count = 0;
+            int[][] checked = new int[ROWS][COLUMNS];
+
+            for(int r=0; r<ROWS-1; r++){
+                for (int c = 0; c < COLUMNS-1; c++) {
+                    if( checked[r][c]==0 && checked[r+1][c]==0 && checked[r][c+1]==0 && checked[r+1][c+1]==0){
+                        if(     shelfie.tile(r+1,c).equals(shelfie.tile(r,c))&&
+                                shelfie.tile(r,c+1).equals(shelfie.tile(r,c))&&
+                                shelfie.tile(r+1,c+1).equals(shelfie.tile(r,c))) {
+                            count++;
+                            checked[r][c]=1;
+                            checked[r+1][c]=1;
+                            checked[r][c+1]=1;
+                            checked[r+1][c+1]=1;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
-            return false;
+            return numSquares(shelfie)>=2;
         }
     },
     THREE_COLUMNS{
@@ -121,8 +149,28 @@ public enum Type implements Serializable {
         }
     },
     EIGHT_EQUAL_TILES{
+        /**
+         *
+         * @param shelfie
+         * @param color
+         * @return how many tiles of a given color are present in the shelfie
+         */
+        public int equalColoredTiles(Shelfie shelfie, Color color ){
+            int count = 0;
+            for(int r=0; r<ROWS; r++){
+                for( int c=0; c<COLUMNS ; c++ ){
+                    if(shelfie.tile(r,c).get().getColor().equals(color))
+                        count++;
+                }
+            }
+            return count;
+        }
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
+            for(Color c : Color.values()){
+                if(equalColoredTiles(shelfie, c)>=8)
+                    return true;
+            }
             return false;
         }
     },
@@ -145,9 +193,31 @@ public enum Type implements Serializable {
         }
     },
     FOUR_ROWS{
+        //SIAMO ARRIVATI QUI
+        /**
+         *
+         * @param shelfie
+         * @param r
+         * @return how many colors are present in a given row of a given shelfie
+         */
+        public int numColorsForRow( Shelfie shelfie, int r){
+            List<Color> count = new ArrayList<Color>();
+            for( int c = 0; c<COLUMNS ; c++ ){
+                if(!count.contains(shelfie.tile(r,c).get().getColor()))
+                    count.add(shelfie.tile(r,c).get().getColor());
+            }
+            return count.size();
+        }
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
-            return false;
+            int count = 0;
+            for( int r=0; r<ROWS ; r++){
+                if( numColorsForRow(shelfie,r)<=3)
+                    count++;
+            }
+            if(count>=4)
+                return true;
+            else return false;
         }
     },
     TWO_ALL_DIFF_COLUMNS{
@@ -175,9 +245,30 @@ public enum Type implements Serializable {
         }
     },
     TWO_ALL_DIFF_ROWS{
+        /**
+         *
+         * @param shelfie
+         * @param r
+         * @return true if the tiles of a given row of a given shelfie are all different, otherwise return false
+         */
+        public boolean isDifferentRow( Shelfie shelfie, int r ){
+            for( int c=0; c<COLUMNS; c++ ){
+                for( int j=c+1; j<COLUMNS; j++){
+                    if(shelfie.tile(r,c).equals(shelfie.tile(r,j)))
+                        return false;
+                }
+            }
+            return true;
+        }
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
-            return false;
+            int count = 0;
+            for( int r=0; r<ROWS; r++ ){
+                if(isDifferentRow(shelfie, r))
+                    count++;
+            }
+            if(count>=2) return true;
+            else return false;
         }
     },
     CROSS{
@@ -198,7 +289,19 @@ public enum Type implements Serializable {
     TRIANGLE{
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
-            return false;
+            if(     shelfie.tile(0,0).get()!=null && shelfie.tile(1,0).get()==null &&
+                    shelfie.tile(1,1).get()!=null && shelfie.tile(2,1).get()==null &&
+                    shelfie.tile(2,2).get()!=null && shelfie.tile(3,2).get()==null &&
+                    shelfie.tile(3,3)!=null && shelfie.tile(4,3)==null &&
+                    shelfie.tile(4,4)!=null
+            ) return true;
+            if(     shelfie.tile(0,4)!=null && shelfie.tile(1,4)==null &&
+                    shelfie.tile(1,3)!=null && shelfie.tile(2,3)==null &&
+                    shelfie.tile(2,2)!=null && shelfie.tile(3,2)==null &&
+                    shelfie.tile(3,1)!=null && shelfie.tile(4,1)==null &&
+                    shelfie.tile(4,0)!=null
+            ) return true;
+            else return false;
         }
     };
 
