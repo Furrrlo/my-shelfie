e98a4aeeb9589fbe2263f84ad295ab6792d11aac
FrancescoGuarda <francesco1.guarda@mail.polimi.it>
2023-03-29 14:23:41 +0200

    created class MatrixGenerator for generating matrixes of colors for testing

---
+++ PSP031-common/src/test/java/it/polimi/ingsw/model/MatrixGenerator.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.model;
+import java.util.Random;
+
+public class MatrixGenerator {
+
+    public MatrixGenerator() {}
+    int ROWS = 6;
+    int COLUMNS = 5;
+    String[] colors = { "Color.GREEN",
+            "Color.YELLOW",
+            "Color.ORANGE",
+            "Color.BLUE",
+            "Color.LIGHTBLUE",
+            "Color.PINK",
+            "null"};
+    public void printMatrix(){
+        Random rand = new Random();
+        System.out.println("Color[][] matrix = {");
+        for( int r=0; r<ROWS; r++ ){
+            String msg = "new Color[] { ";
+            for( int c=0; c<COLUMNS-1; c++){
+                msg = msg + String.format("%-15s", colors[rand.nextInt(colors.length)]) + ", ";
+            }
+            msg = msg + String.format("%-15s", colors[rand.nextInt(colors.length)]) + " },";
+            System.out.println(msg);
+        }
+    }
+
+    public static void main(String[] args) {
+        MatrixGenerator m = new MatrixGenerator();
+
+        m.printMatrix();
+    }
+}
+
