9f0713962ba2c5ef83a826782924ac31bd5ca760
jackorse <giacomo.orsenigo@gmail.com>
2023-03-30 14:23:58 +0200

    Print already formatted matrix in MatrixGenerator

--- PSP031-common/src/test/java/it/polimi/ingsw/model/MatrixGenerator.java
+++ PSP031-common/src/test/java/it/polimi/ingsw/model/MatrixGenerator.java
@@ -18,23 +18,25 @@ public class MatrixGenerator {
             "Color.PINK",
             "null"
     };
+    String INDENT = "        ";
 
     public void printMatrix() {
         Random rand = new Random();
         System.out.println("Color[][] matrix = {");
+        System.out.println(INDENT + "//@formatter:off");
         for (int r = 0; r < ROWS; r++) {
-            StringBuilder msg = new StringBuilder("new Color[] { ");
+            StringBuilder msg = new StringBuilder(INDENT + "new Color[] { ");
             for (int c = 0; c < COLUMNS - 1; c++) {
                 msg.append(String.format("%-15s", colors[rand.nextInt(colors.length)])).append(", ");
             }
             msg.append(String.format("%-15s", colors[rand.nextInt(colors.length)])).append(" },");
             System.out.println(msg);
         }
+        System.out.println(INDENT + "//@formatter:on\n};");
     }
 
     public static void main(String[] args) {
         MatrixGenerator m = new MatrixGenerator();
-
         m.printMatrix();
     }
 }
