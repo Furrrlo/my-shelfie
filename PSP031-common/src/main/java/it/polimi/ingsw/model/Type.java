20b0a185a0b307ad7bb12f8e31a8b3f548449a6f
FrancescoGuarda <francesco1.guarda@mail.polimi.it>
2023-04-03 20:57:51 +0200

    implemented printCommonGoal for Type.FOUR_ROWS, .TWO_ALL_DIFF_COLUMNS, .TWO_ALL_DIFF_ROWS, .CROSS, .TRIANGLE and solved all warnings

--- PSP031-common/src/main/java/it/polimi/ingsw/model/Type.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/model/Type.java
@@ -65,7 +65,6 @@ public enum Type implements Serializable {
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
             int count = 0;
-            boolean achieved = false;
             int[][] checked = new int[ROWS][COLUMNS];
             for (int r = 0; r < ROWS; r++) {
                 for (int c = 0; c < COLUMNS; c++) {
@@ -75,7 +74,7 @@ public enum Type implements Serializable {
                     }
                 }
             }
-            achieved = count >= 4;
+            boolean achieved = count >= 4;
             if (achieved)
                 printCommonGoal(shelfie, checked, "FOUR QUADRIPLETS");
             return achieved;
@@ -90,7 +89,7 @@ public enum Type implements Serializable {
             List<Index> indexes = new ArrayList<>();
             indexes.add(new Index(row, col));
             checked[row][col] = marker;
-            int prevSize = 0;
+            int prevSize;
             do {
                 prevSize = indexes.size();
                 for (int i = 0; i < indexes.size(); i++) {
@@ -158,9 +157,8 @@ public enum Type implements Serializable {
 
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
-            boolean achieved = false;
             int[][] checked = new int[ROWS][COLUMNS];
-            achieved = numSquares(shelfie, checked) >= 2;
+            boolean achieved = numSquares(shelfie, checked) >= 2;
             if (achieved)
                 printCommonGoal(shelfie, checked, "TWO SQUARES");
             return achieved;
@@ -193,13 +191,12 @@ public enum Type implements Serializable {
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
             int count = 0;
-            boolean achieved = false;
             int[][] checked = new int[ROWS][COLUMNS];
             for (int c = 0; c < COLUMNS; c++) {
                 if (numColorsForColumn(shelfie, c, checked, count + 1) <= 3)
                     count++;
             }
-            achieved = (count >= 3);
+            boolean achieved = (count >= 3);
             if (achieved)
                 printCommonGoal(shelfie, checked, "THREE COLUMNS");
             return achieved;
@@ -283,7 +280,7 @@ public enum Type implements Serializable {
         /**
          * Returns the number of different colors present in a given row r of a shelfie, excluding null tiles
          */
-        public int numColorsForRow(Shelfie shelfie, int r) {
+        public int numColorsForRow(Shelfie shelfie, int[][] checked, int marker, int r) {
             List<Color> count = new ArrayList<>();
             for (int c = 0; c < COLUMNS; c++) {
                 //if there is at least a null tile the row doesn't count because it must be full
@@ -292,16 +289,22 @@ public enum Type implements Serializable {
                 if (!count.contains(Objects.requireNonNull(shelfie.tile(r, c).get()).getColor()))
                     count.add(Objects.requireNonNull(shelfie.tile(r, c).get()).getColor());
             }
+            if (count.size() < 4)
+                for (int c = 0; c < COLUMNS; c++)
+                    checked[r][c] = marker;
             return count.size();
         }
 
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
             int count = 0;
+            int[][] checked = new int[ROWS][COLUMNS];
             for (int r = 0; r < ROWS; r++) {
-                if (numColorsForRow(shelfie, r) <= 3)
+                if (numColorsForRow(shelfie, checked, count + 1, r) <= 3)
                     count++;
             }
+            if (count >= 4)
+                printCommonGoal(shelfie, checked, "FOUR ROWS");
             return count >= 4;
         }
     },
@@ -310,23 +313,29 @@ public enum Type implements Serializable {
         /**
          * Returns the number of different colors present in a given color c of a shelfie, excluding null tiles
          */
-        public int numColorsForColumn(Shelfie shelfie, int c) {
+        public int numColorsForColumn(Shelfie shelfie, int[][] checked, int marker, int c) {
             List<Color> count = new ArrayList<>();
             for (int r = 0; r < ROWS; r++) {
                 if (shelfie.tile(r, c).get() != null
                         && !count.contains(Objects.requireNonNull(shelfie.tile(r, c).get()).getColor()))
                     count.add(Objects.requireNonNull(shelfie.tile(r, c).get()).getColor());
             }
+            if (count.size() == 6)
+                for (int r = 0; r < ROWS; r++)
+                    checked[r][c] = marker;
             return count.size();
         }
 
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
             int count = 0;
+            int[][] checked = new int[ROWS][COLUMNS];
             for (int c = 0; c < COLUMNS; c++) {
-                if (numColorsForColumn(shelfie, c) == ROWS)
+                if (numColorsForColumn(shelfie, checked, count + 1, c) == ROWS)
                     count++;
             }
+            if (count >= 2)
+                printCommonGoal(shelfie, checked, "TWO ALL DIFFERENT COLUMNS");
             return count >= 2;
         }
     },
@@ -334,65 +343,104 @@ public enum Type implements Serializable {
         /**
          * Returns the number of different colors present in a given row r of a shelfie, excluding null tiles
          */
-        public int numColorsForRow(Shelfie shelfie, int r) {
+        public int numColorsForRow(Shelfie shelfie, int[][] checked, int marker, int r) {
             List<Color> count = new ArrayList<>();
             for (int c = 0; c < COLUMNS; c++) {
                 if (shelfie.tile(r, c).get() != null
                         && !count.contains(Objects.requireNonNull(shelfie.tile(r, c).get()).getColor()))
                     count.add(Objects.requireNonNull(shelfie.tile(r, c).get()).getColor());
             }
+            if (count.size() == 5)
+                for (int c = 0; c < COLUMNS; c++)
+                    checked[r][c] = marker;
             return count.size();
         }
 
         @Override
         public boolean checkCommonGoal(Shelfie shelfie) {
             int count = 0;
+            int[][] checked = new int[ROWS][COLUMNS];
             for (int r = 0; r < ROWS; r++) {
-                if (numColorsForRow(shelfie, r) == COLUMNS)
+                if (numColorsForRow(shelfie, checked, count + 1, r) == COLUMNS)
                     count++;
             }
+            if (count >= 2)
+                printCommonGoal(shelfie, checked, "TWO ALL DIFFERENT ROWS");
             return count >= 2;
         }
     },
     CROSS {
-        @Override
-        public boolean checkCommonGoal(Shelfie shelfie) {
+        public boolean checkForCross(Shelfie shelfie, int[][] checked) {
+            int count = 0;
             for (int r = 0; r < ROWS - 2; r++) {
                 for (int c = 0; c < COLUMNS - 2; c++) {
                     if (shelfie.tile(r, c).get() != null &&
                             shelfie.tile(r, c).equals(shelfie.tile(r + 2, c)) &&
                             shelfie.tile(r, c).equals(shelfie.tile(r, c + 2)) &&
                             shelfie.tile(r, c).equals(shelfie.tile(r + 1, c + 1)) &&
-                            shelfie.tile(r, c).equals(shelfie.tile(r + 2, c + 2)))
-                        return true;
+                            shelfie.tile(r, c).equals(shelfie.tile(r + 2, c + 2))) {
+                        count++;
+                        checked[r][c] = count;
+                        checked[r + 2][c] = count;
+                        checked[r][c + 2] = count;
+                        checked[r + 2][c + 2] = count;
+                        checked[r + 1][c + 1] = count;
+                    }
                 }
             }
-            return false;
+            return count > 0;
+        }
+
+        @Override
+        public boolean checkCommonGoal(Shelfie shelfie) {
+            int[][] checked = new int[ROWS][COLUMNS];
+            boolean achieved = checkForCross(shelfie, checked);
+            if (achieved)
+                printCommonGoal(shelfie, checked, "CROSS");
+            return achieved;
         }
     },
     TRIANGLE {
-        @Override
-        public boolean checkCommonGoal(Shelfie shelfie) {
-
+        public boolean checkForTriangle(Shelfie shelfie, int[][] checked) {
+            int count = 0;
             for (int r = 0; r < 2; r++) {
                 if (shelfie.tile(r, 0).get() != null && shelfie.tile(r + 1, 0).get() == null &&
                         shelfie.tile(r + 1, 1).get() != null && shelfie.tile(r + 2, 1).get() == null &&
                         shelfie.tile(r + 2, 2).get() != null && shelfie.tile(r + 3, 2).get() == null &&
                         shelfie.tile(r + 3, 3).get() != null && shelfie.tile(r + 4, 3).get() == null &&
-                        shelfie.tile(r + 4, 4).get() != null)
-                    return true;
+                        shelfie.tile(r + 4, 4).get() != null) {
+                    count++;
+                    checked[r][0] = count;
+                    checked[r + 1][1] = count;
+                    checked[r + 2][2] = count;
+                    checked[r + 3][3] = count;
+                    checked[r + 4][4] = count;
+                }
             }
             for (int r = 0; r < 2; r++) {
                 if (shelfie.tile(r, 4).get() != null && shelfie.tile(r + 1, 4).get() == null &&
                         shelfie.tile(r + 1, 3).get() != null && shelfie.tile(r + 2, 3).get() == null &&
                         shelfie.tile(r + 2, 2).get() != null && shelfie.tile(r + 3, 2).get() == null &&
                         shelfie.tile(r + 3, 1).get() != null && shelfie.tile(r + 4, 1).get() == null &&
-                        shelfie.tile(r + 4, 0).get() != null)
-                    return true;
+                        shelfie.tile(r + 4, 0).get() != null) {
+                    count++;
+                    checked[r][4] = count;
+                    checked[r + 1][3] = count;
+                    checked[r + 2][2] = count;
+                    checked[r + 3][1] = count;
+                    checked[r + 4][0] = count;
+                }
             }
+            return count > 0;
+        }
 
-            return false;
-
+        @Override
+        public boolean checkCommonGoal(Shelfie shelfie) {
+            int[][] checked = new int[ROWS][COLUMNS];
+            boolean achieved = checkForTriangle(shelfie, checked);
+            if (achieved)
+                printCommonGoal(shelfie, checked, "TRIANGLE");
+            return achieved;
         }
     };
 
