06212190000267e0d4c98deb78b7af749f0648f6
giovannialbertos@hotmail.it <giovannialbertos@hotmail.it>
2023-04-24 23:01:33 +0200

    added makeMove method to GameServerController

--- PSP031-server/src/main/java/it/polimi/ingsw/server/controller/GameServerController.java
+++ PSP031-server/src/main/java/it/polimi/ingsw/server/controller/GameServerController.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.server.controller;
 
 import it.polimi.ingsw.BoardCoord;
+import it.polimi.ingsw.model.*;
 import it.polimi.ingsw.server.model.ServerGame;
 import it.polimi.ingsw.server.model.ServerPlayer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings({ "FieldCanBeLocal", "unused" })
@@ -28,6 +30,26 @@ public class GameServerController {
     public void makeMove(ServerPlayer player, List<BoardCoord> selected, int shelfCol) throws IllegalArgumentException {
         try (var gameCloseable = game.use()) {
             var game = gameCloseable.obj();
+            List <Property<Tile>> selectedTiles = new ArrayList<>();
+            if (!game.getBoard().checkBoardCoord(selected) || !player.getShelfie().checkColumnSpace(shelfCol,selected.size())) {
+                throw new IllegalArgumentException("Invalid move");
+            }
+
+            //remove tiles from board
+            for (BoardCoord coord : selected) {
+                selectedTiles.add(game.getBoard().tile(coord.row(), coord.col()));
+                game.getBoard().removeTile(coord.row(), coord.col());
+                }
+            
+            //add tiles to shelfie
+            player.getShelfie().placeTiles(selectedTiles, shelfCol);
+
+            if (game.getBoard().isEmpty())
+            {
+                game.getBoard().refillBoard();
+            }
+
+           
 
         }
     }
