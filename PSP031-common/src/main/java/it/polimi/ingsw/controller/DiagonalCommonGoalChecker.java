e9662309edb4b5403f3f83810b228bf307448708
Furrrlo <ferloo@protonmail.com>
2023-04-27 21:20:22 +0200

    Fix DiagonalCommonGoalChecker AIOB exception

--- PSP031-common/src/main/java/it/polimi/ingsw/controller/DiagonalCommonGoalChecker.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/controller/DiagonalCommonGoalChecker.java
@@ -19,6 +19,7 @@ public class DiagonalCommonGoalChecker implements CommonGoalChecker {
     public boolean checkDiagonal(Shelfie shelfie, int[][] checked, int r, int c) {
         if (r >= ROWS - 4 || !(c == 0 || c == COLUMNS - 1) || shelfie.tile(r, c).get() == null)
             return false;
+
         if (c == 0) {
             for (int i = 0; i < 4; i++) {
                 if (!Objects.equals(shelfie.tile(r, c).get(), shelfie.tile(r + i, c + i).get()))
@@ -28,11 +29,11 @@ public class DiagonalCommonGoalChecker implements CommonGoalChecker {
                 checked[r + i][c + i] = 1;
         } else {
             for (int i = 0; i > -4; i--) {
-                if (!Objects.equals(shelfie.tile(r, c).get(), shelfie.tile(r + i, c + i).get()))
+                if (!Objects.equals(shelfie.tile(r, c).get(), shelfie.tile(r - i, c + i).get()))
                     return false;
             }
             for (int i = 0; i >= -4; i--)
-                checked[r + i][c + i] = 1;
+                checked[r - i][c + i] = 1;
         }
         return true;
     }
