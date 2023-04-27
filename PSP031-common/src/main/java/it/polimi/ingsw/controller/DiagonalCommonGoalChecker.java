0b7986193bb1498957e3c5a00c46dda3d3a143f3
Furrrlo <ferloo@protonmail.com>
2023-04-27 21:26:53 +0200

    Fix DiagonalCommonGoalChecker off by one errors

--- PSP031-common/src/main/java/it/polimi/ingsw/controller/DiagonalCommonGoalChecker.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/controller/DiagonalCommonGoalChecker.java
@@ -16,23 +16,24 @@ public class DiagonalCommonGoalChecker implements CommonGoalChecker {
      * Returns true if the diagonal in the shelfie built from tile in position r,c is made by the same colored
      * tiles, and it's made by exactly 5 tiles, otherwise returns false
      */
-    public boolean checkDiagonal(Shelfie shelfie, int[][] checked, int r, int c) {
-        if (r >= ROWS - 4 || !(c == 0 || c == COLUMNS - 1) || shelfie.tile(r, c).get() == null)
+    private boolean checkDiagonal(Shelfie shelfie, int[][] checked, int r, int c) {
+        final int diagonalSize = Math.min(ROWS, COLUMNS);
+        if (r > ROWS - diagonalSize || !(c == 0 || c == COLUMNS - 1) || shelfie.tile(r, c).get() == null)
             return false;
 
         if (c == 0) {
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < diagonalSize; i++) {
                 if (!Objects.equals(shelfie.tile(r, c).get(), shelfie.tile(r + i, c + i).get()))
                     return false;
             }
-            for (int i = 0; i <= 4; i++)
+            for (int i = 0; i < diagonalSize; i++)
                 checked[r + i][c + i] = 1;
         } else {
-            for (int i = 0; i > -4; i--) {
+            for (int i = 0; i > -diagonalSize; i--) {
                 if (!Objects.equals(shelfie.tile(r, c).get(), shelfie.tile(r - i, c + i).get()))
                     return false;
             }
-            for (int i = 0; i >= -4; i--)
+            for (int i = 0; i > -diagonalSize; i--)
                 checked[r - i][c + i] = 1;
         }
         return true;
