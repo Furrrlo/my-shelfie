dbd505908720dff0e9bde15ad175a2ce3051e29a
jackorse <giacomo.orsenigo@gmail.com>
2023-04-27 18:41:19 +0200

    Add printBoard
    We can't use TuiPrinter yet because it's too big

--- PSP031-client/src/main/java/it/polimi/ingsw/client/tui/TuiGameScene.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/tui/TuiGameScene.java
@@ -113,4 +113,44 @@ class TuiGameScene {
             out.println(msg);
         }
     }
+
+    public static void printBoard(TuiPrintStream out, BoardView board) {
+        for (int row = 0; row < BoardView.BOARD_ROWS; row++) {
+            StringBuilder msg = new StringBuilder();
+            if (row == 0) {
+                msg.append(' ');
+                for (int i = 1; i <= BoardView.BOARD_COLUMNS; i++)
+                    msg.append(' ').append(i).append(' ');
+                msg.append('\n');
+            }
+            for (int col = 0; col < BoardView.BOARD_COLUMNS; col++) {
+                if (col == 0)
+                    msg.append(row + 1).append(" ");
+
+                if (board.isValidTile(row, col)) {
+                    @Nullable
+                    Tile tile = board.tile(row, col).get();
+                    if (tile != null) {
+                        switch (tile.getColor()) {
+                            case BLUE -> msg.append(ConsoleColors.CYAN).append(ConsoleColors.BLUE_BACKGROUND_BRIGHT);
+                            case GREEN -> msg.append(ConsoleColors.GREEN).append(ConsoleColors.GREEN_BACKGROUND_BRIGHT);
+                            case YELLOW -> msg.append(ConsoleColors.YELLOW_BRIGHT)
+                                    .append(ConsoleColors.ORANGE_BACKGROUND_BRIGHT);
+                            case PINK -> msg.append(ConsoleColors.PURPLE).append(ConsoleColors.PURPLE_BACKGROUND_BRIGHT);
+                            case WHITE -> msg.append(ConsoleColors.ORANGE).append(ConsoleColors.YELLOW_BACKGROUND_BRIGHT);
+                            case LIGHTBLUE -> msg.append(ConsoleColors.BLUE).append(ConsoleColors.CYAN_BACKGROUND_BRIGHT);
+                        }
+                    } else {
+                        msg.append(ConsoleColors.BLACK).append(ConsoleColors.BLACK_BACKGROUND_BRIGHT);
+                    }
+                } else {
+                    msg.append(ConsoleColors.BLACK).append(ConsoleColors.BLACK_BACKGROUND_BRIGHT);
+                }
+
+                msg.append("   ").append(ConsoleColors.RESET);
+            }
+            out.println(msg);
+        }
+
+    }
 }
