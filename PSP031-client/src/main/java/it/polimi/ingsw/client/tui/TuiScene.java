722065cf887d90b39245dad68edd608a1d894b64
Furrrlo <ferloo@protonmail.com>
2023-05-09 14:59:16 +0200

    Add javadocs to TuiScene

--- PSP031-client/src/main/java/it/polimi/ingsw/client/tui/TuiScene.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/tui/TuiScene.java
@@ -1,9 +1,23 @@
 package it.polimi.ingsw.client.tui;
 
+/**
+ * Represents a scene to be rendered by a {@link TuiRenderer}
+ * <p/>
+ * A scene represents an entire game screen, excluding the prompt. So a single scene can render the same content,
+ * but have different prompts to allow the user to make different actions
+ * 
+ * @see TuiRenderer#setScene(TuiScene)
+ */
 public interface TuiScene {
 
+    /**
+     * Renders the current scene to the given output stream
+     * 
+     * @param out the output stream to render to
+     */
     void render(TuiPrintStream out);
 
+    /** Hook invoked by {@link TuiRenderer} when a scene is replaced by another one */
     default void close() {
     }
 }
