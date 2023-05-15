baf136a400ce963b778c7662f4327bacbf3a3cec
FrancescoGuarda <francesco1.guarda@mail.polimi.it>
2023-05-15 20:50:42 +0200

    modified UserMessage so that implements Serializable

--- PSP031-common/src/main/java/it/polimi/ingsw/model/UserMessage.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/model/UserMessage.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model;
 
-public record UserMessage(String nickSendingPlayer, String message, String nickReceivingPlayer) {
+import java.io.Serializable;
+
+public record UserMessage(String nickSendingPlayer, String message, String nickReceivingPlayer) implements Serializable {
     @Override
     public String toString() {
         return '[' + nickSendingPlayer + "]: " + message;
