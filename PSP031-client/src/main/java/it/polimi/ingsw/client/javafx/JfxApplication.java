48cc4be05d09413ff1915c0a0443fe4f19f48f4a
giovanni <giovannialbertos@hotmail.it>
2023-05-23 16:37:18 +0200

    added mainmenu

--- PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/JfxApplication.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/JfxApplication.java
@@ -1,38 +1,16 @@
 package it.polimi.ingsw.client.javafx;
 
-import it.polimi.ingsw.GameAndController;
-import it.polimi.ingsw.client.network.socket.SocketClientNetManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.concurrent.CompletableFuture;
-
 public class JfxApplication extends Application {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(JfxApplication.class);
 
     @Override
     public void start(Stage stage) throws Exception {
-        var netManager = SocketClientNetManager.connect(new InetSocketAddress(1234), "jfx");
-        var lobbyAndController = netManager.joinGame();
-        lobbyAndController.controller().ready(true);
 
-        GameAndController<?> gameAndController;
-        if ((gameAndController = lobbyAndController.lobby().game().get()) == null) {
-            final CompletableFuture<GameAndController<?>> gameAndControllerFuture = new CompletableFuture<>();
-            lobbyAndController.lobby().game().registerObserver(gameAndControllerFuture::complete);
-            gameAndController = gameAndControllerFuture.get();
-        }
-
-        Scene scene = new JfxGameScene(gameAndController.game(), gameAndController.controller());
+        Scene scene = new JfxMainMenuScene(stage);
 
         stage.setTitle("My Shelfie");
 
@@ -46,17 +24,5 @@ public class JfxApplication extends Application {
         stage.setMinHeight(500);
         stage.setHeight(500);
         stage.show();
-        stage.setOnCloseRequest(e -> {
-            int exitCode = 0;
-            try {
-                netManager.close();
-            } catch (IOException ex) {
-                LOGGER.error("Failed to disconnect from the server while closing", ex);
-                exitCode = -1;
-            }
-
-            Platform.exit();
-            System.exit(exitCode);
-        });
     }
 }
---
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/JfxMainMenuScene.java
@@ -0,0 +1,172 @@
+package it.polimi.ingsw.client.javafx;
+
+import it.polimi.ingsw.GameAndController;
+import it.polimi.ingsw.client.network.ClientNetManager;
+import it.polimi.ingsw.client.network.rmi.RmiClientNetManager;
+import it.polimi.ingsw.client.network.socket.SocketClientNetManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+
+public class JfxMainMenuScene extends Scene {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JfxMainMenuScene.class);
+
+    public JfxMainMenuScene(Stage stage) {
+        super(createRootNode(stage));
+    }
+
+    private static Parent createRootNode(Stage stage) {
+        //var mainPane = new CenteringFitPane();
+        //mainPane.getChildren().add(new MainMenuPane());
+        //Pattern ipPattern = Pattern
+        //                .compile(" (\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3} ");
+
+        // Create labels
+        Label connectionTypeLabel = new Label("Connection Type:");
+        Label ipLabel = new Label("IP:");
+        Label portLabel = new Label("Port:");
+        Label usernameLabel = new Label("Username:");
+
+        // Create choice dialog for connection type
+        ChoiceBox<String> connectionTypeChoice = new ChoiceBox<>();
+        connectionTypeChoice.getItems().addAll("RMI", "Socket");
+        connectionTypeChoice.getSelectionModel().selectFirst();
+
+        // Create text fields
+        TextField ipTextField = new TextField();
+        TextField portTextField = new TextField();
+        TextField usernameTextField = new TextField();
+
+        //create tile image
+        Image titleImage = new Image(FxResources.getResourceAsStream("assets/Publisher material/Title 2000x618px.png"), 400,
+                124, true, false);
+        ImageView titleView = new ImageView(titleImage);
+
+        // Create grid pane for layout
+        GridPane mainPane = new GridPane();
+        mainPane.setHgap(10);
+        mainPane.setVgap(10);
+        mainPane.setPadding(new Insets(10));
+
+        // Add components to grid pane
+        mainPane.add(connectionTypeLabel, 0, 0);
+        mainPane.add(connectionTypeChoice, 1, 0);
+        mainPane.add(ipLabel, 0, 1);
+        mainPane.add(ipTextField, 1, 1);
+        mainPane.add(portLabel, 0, 2);
+        mainPane.add(portTextField, 1, 2);
+        mainPane.add(usernameLabel, 0, 3);
+        mainPane.add(usernameTextField, 1, 3);
+        mainPane.setAlignment(Pos.CENTER);
+
+        //ip validation
+        Label isIpValid = new Label("");
+        EventHandler<ActionEvent> eventIpCHeck = e -> {
+            String ipText = ipTextField.getText();
+            String portText = portTextField.getText();
+            String usernameText = usernameTextField.getText();
+            String connectionType = connectionTypeChoice.getValue();
+
+            try {
+                ClientNetManager netManager;
+                if (connectionType.equals("RMI")) {
+                    netManager = RmiClientNetManager.connect(ipText, Integer.parseInt(portText), usernameText);
+                } else if (connectionType.equals("Socket")) {
+                    netManager = SocketClientNetManager.connect(new InetSocketAddress(ipText, Integer.parseInt(portText)),
+                            usernameText);
+                } else {
+                    throw new RuntimeException();
+                }
+                var lobbyAndController = netManager.joinGame();
+                lobbyAndController.controller().ready(true);
+
+                GameAndController<?> gameAndController;
+                if ((gameAndController = lobbyAndController.lobby().game().get()) == null) {
+                    final CompletableFuture<GameAndController<?>> gameAndControllerFuture = new CompletableFuture<>();
+                    lobbyAndController.lobby().game().registerObserver(gameAndControllerFuture::complete);
+                    gameAndController = gameAndControllerFuture.get();
+                }
+
+                Scene scene = new JfxGameScene(gameAndController.game(), gameAndController.controller());
+
+                stage.setTitle("My Shelfie");
+
+                // Let jfx pick the best fit
+                stage.getIcons()
+                        .add(new Image(FxResources.getResourceAsStream("assets/Publisher material/Icon 50x50px.png")));
+                stage.getIcons()
+                        .add(new Image(FxResources.getResourceAsStream("assets/Publisher material/Box 280x280px.png")));
+
+                stage.setScene(scene);
+                stage.setMinWidth(800);
+                stage.setWidth(1080);
+                stage.setMinHeight(500);
+                stage.setHeight(720);
+                stage.show();
+                stage.setOnCloseRequest(exit -> {
+                    int exitCode = 0;
+                    try {
+                        netManager.close();
+                    } catch (IOException ex) {
+                        LOGGER.error("Failed to disconnect from the server while closing", ex);
+                        exitCode = -1;
+                    }
+
+                    Platform.exit();
+                    System.exit(exitCode);
+                });
+
+            } catch (Exception ignored) {
+            }
+        };
+
+        // Create start button
+        Button startButton = new Button("Connect");
+        startButton.setOnAction(eventIpCHeck);
+
+        //vbox
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+        vbox.setSpacing(10);
+        vbox.getChildren().addAll(titleView, mainPane, startButton, isIpValid);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10d);
+
+        AnchorPane anchorPane = new AnchorPane();
+
+        AnchorPane.setTopAnchor(vbox, 1d);
+        AnchorPane.setBottomAnchor(vbox, 10d);
+        AnchorPane.setLeftAnchor(vbox, 10d);
+        AnchorPane.setRightAnchor(vbox, 10d);
+        anchorPane.getChildren().add(vbox);
+        //anchorPane.prefWidthProperty().bind(scene.widthProperty());
+        //anchorPane.prefHeightProperty().bind(scene.heightProperty());
+
+        anchorPane.setBackground(new Background(new BackgroundImage(
+                new Image(FxResources.getResourceAsStream("assets/misc/sfondo parquet.jpg")),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(100, 100, true, true, false, true))));
+
+        return anchorPane;
+    }
+}
