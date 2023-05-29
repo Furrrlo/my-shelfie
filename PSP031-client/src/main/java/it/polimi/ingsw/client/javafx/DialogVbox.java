e3c6c8bdace7452ad51842bef9d075322cc9684c
FrancescoGuarda <francesco1.guarda@mail.polimi.it>
2023-05-29 20:31:54 +0200

    refined DialogVbox

--- PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/DialogVbox.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/DialogVbox.java
@@ -4,13 +4,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 
 public class DialogVbox extends VBox {
 
@@ -18,11 +16,11 @@ public class DialogVbox extends VBox {
     public static int DISCONNECTED = 2;
 
     public DialogVbox(int type) {
-        setSpacing(10);
+        setSpacing(15);
         backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
                 Color.LIGHTGRAY,
                 new CornerRadii(Math.min(10, 10 * (width.doubleValue() / 210d))),
-                new Insets(0)))));
+                new Insets(-10)))));
         String errorTitle = "";
         switch (type) {
             case 1 -> errorTitle = "Player has disconnected";
@@ -32,16 +30,17 @@ public class DialogVbox extends VBox {
         Label errorName = new Label("Error : " + errorTitle);
         errorName.setFont(Font.font(Font.getDefault().getName(), FontWeight.EXTRA_BOLD, Font.getDefault().getSize()));
         errorName.backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
-                Color.RED,
-                new CornerRadii(Math.min(10, 10 * (width.doubleValue() / 210d))),
-                new Insets(0)))));
+                Color.LIGHTSEAGREEN,
+                new CornerRadii(Math.min(5, 5 * (width.doubleValue() / 210d))),
+                new Insets(-5)))));
         errorName.setAlignment(Pos.CENTER);
+        errorName.prefWidthProperty().bind(this.widthProperty());
         this.getChildren().add(errorName);
 
         String errorMessage = "";
         switch (type) {
-            case 1 -> errorMessage = "One or more player have disconnected, the game will resume only once all the " +
-                    "disconnected players have rejoined the game, or more than 30 second have passed";
+            case 1 -> errorMessage = "The game is suspended because all other players have disconnected.\n" +
+                    "If no one reconnects within 30 seconds, the game will end";
             case 2 -> errorMessage = "It's not your current turn, please wait when is your turn before you can " +
                     "select tiles from board again";
             default -> errorMessage = "Error";
@@ -50,14 +49,26 @@ public class DialogVbox extends VBox {
         dialogue.setWrapText(true);
         dialogue.backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
                 Color.WHITE,
-                new CornerRadii(Math.min(10, 10 * (width.doubleValue() / 210d))),
-                new Insets(0)))));
+                new CornerRadii(Math.min(5, 5 * (width.doubleValue() / 210d))),
+                new Insets(-5)))));
+        dialogue.setAlignment(Pos.CENTER);
+        dialogue.setTextAlignment(TextAlignment.CENTER);
+        dialogue.setGraphicTextGap(20);
         this.getChildren().add(dialogue);
+        dialogue.prefWidthProperty().bind(widthProperty());
+        dialogue.prefHeightProperty().bind(heightProperty());
 
+        HBox hbox = new HBox();
+        hbox.prefWidthProperty().bind(widthProperty());
         Button ok = new Button("ok");
-        ok.setAlignment(Pos.BOTTOM_RIGHT);
-        ok.setBackground(Background.fill(Color.LIGHTSEAGREEN));
+        ok.setAlignment(Pos.CENTER_RIGHT);
+        hbox.getChildren().add(ok);
+        hbox.setAlignment(Pos.CENTER_RIGHT);
+        ok.backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
+                Color.LIGHTSEAGREEN,
+                new CornerRadii(Math.min(5, 5 * (width.doubleValue() / 210d))),
+                new Insets(-5)))));
         ok.setOnMouseClicked(e -> this.setVisible(false));
-        this.getChildren().add(ok);
+        this.getChildren().add(hbox);
     }
 }
