0869b33d4453e9a43f36292d9c3aacc4d8de02a8
jackorse <giacomo.orsenigo@gmail.com>
2023-06-01 17:09:09 +0200

    Enable logging in jfx

--- PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/JfxMain.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/JfxMain.java
@@ -1,7 +1,17 @@
 package it.polimi.ingsw.client.javafx;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
 class JfxMain {
     public static void main(String[] args) {
+        // Configure log4j file if none is already set
+        if (System.getProperty("log4j.configurationFile") == null)
+            System.setProperty("log4j.configurationFile", "log4j2-tui.xml");
+
+        // add SLF4JBridgeHandler to j.u.l's root logger
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
         JfxApplication.launch(JfxApplication.class, args);
     }
 }
