5af01d49a42d00e71e484cf0c185369a8ff5081d
Furrrlo <ferloo@protonmail.com>
2023-06-07 20:02:42 +0200

    Cleanup JoinGameTest a bit

--- PSP031-integration-tests/src/test/java/it/polimi/ingsw/JoinGameTest.java
+++ PSP031-integration-tests/src/test/java/it/polimi/ingsw/JoinGameTest.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.client.network.ClientNetManager;
 import it.polimi.ingsw.client.network.rmi.RmiClientNetManager;
 import it.polimi.ingsw.client.network.socket.SocketClientNetManager;
 import it.polimi.ingsw.controller.NickNotValidException;
@@ -23,62 +22,71 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.rmi.registry.LocateRegistry;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Disabled // TODO: investigate why they deadlock
 public class JoinGameTest {
+
+    private static final Function<JoinGameTest, TestClientNetManagerFactory> rmiFunction = f -> f.rmiClientFactory;
+    private static final Function<JoinGameTest, TestClientNetManagerFactory> socketFunction = f -> f.socketClientFactory;
+
     private volatile ServerController serverController;
-    private final AtomicInteger choosenPort = new AtomicInteger();
-    private volatile RMIPortCapturingServerSocketFactory portCapturingServerSocketFactory;
-    private volatile String remoteName;
     private volatile SocketConnectionServerController socketConnectionServerController;
     private volatile RmiConnectionServerController rmiConnectionServerController;
+    private volatile CloseablesTracker clientCloseables;
 
-    private static final TestClientNetManagerFactory rmiFunction = (f, nick) -> RmiClientNetManager.connect(
-            null,
-            f.portCapturingServerSocketFactory.getFirstCapturedPort(),
-            f.remoteName,
-            nick);
-    private static final TestClientNetManagerFactory socketFunction = (f, nick) -> SocketClientNetManager.connect(
-            new InetSocketAddress(InetAddress.getLocalHost(), f.choosenPort.get()),
-            1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS,
-            nick);
+    private volatile TestClientNetManagerFactory rmiClientFactory;
+    private volatile TestClientNetManagerFactory socketClientFactory;
 
     private interface TestClientNetManagerFactory {
 
-        ClientNetManager create(JoinGameTest test, String nick) throws Exception;
+        LobbyAndController<? extends LobbyView> createAndJoin(String nick) throws Exception;
     }
 
     @BeforeEach
     void setUp() throws IOException {
-        remoteName = "rmi_e2e_" + System.currentTimeMillis();
-        var serverSocket = new ServerSocket(0);
-        choosenPort.set(serverSocket.getLocalPort());
+        final var serverSocket = new ServerSocket(0);
+        final var choosenPort = serverSocket.getLocalPort();
         serverController = new ServerController(500, TimeUnit.MILLISECONDS);
         socketConnectionServerController = new SocketConnectionServerController(serverController, serverSocket,
                 -1, TimeUnit.MILLISECONDS,
                 1, TimeUnit.SECONDS);
-        portCapturingServerSocketFactory = new RMIPortCapturingServerSocketFactory();
+
+        final var remoteName = "rmi_e2e_" + System.currentTimeMillis();
+        final var portCapturingServerSocketFactory = new RMIPortCapturingServerSocketFactory();
         rmiConnectionServerController = RmiConnectionServerController.bind(
                 LocateRegistry.createRegistry(0, null, portCapturingServerSocketFactory),
                 remoteName,
                 serverController);
+
+        clientCloseables = new CloseablesTracker();
+        rmiClientFactory = nick -> clientCloseables.register(RmiClientNetManager.connect(
+                null,
+                portCapturingServerSocketFactory.getFirstCapturedPort(),
+                remoteName,
+                nick)).joinGame();
+        socketClientFactory = nick -> clientCloseables.register(SocketClientNetManager.connect(
+                new InetSocketAddress(InetAddress.getLocalHost(), choosenPort),
+                1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS,
+                nick)).joinGame();
     }
 
     @AfterEach
+    @SuppressWarnings("EmptyTryBlock")
     void tearDown() throws IOException {
-        serverController.close();
-        socketConnectionServerController.close();
-        rmiConnectionServerController.close();
+        try (var ignored1 = serverController;
+             var ignored2 = socketConnectionServerController;
+             var ignored3 = rmiConnectionServerController;
+             var ignored4 = clientCloseables;) {
+            // Close all of them, but try even if one fails
+        }
     }
 
     public static Stream<Arguments> twoPlayersTest() {
@@ -91,23 +99,23 @@ public class JoinGameTest {
 
     @ParameterizedTest
     @MethodSource
-    void twoPlayersTest(TestClientNetManagerFactory clientNetManagerFactory1,
-                        TestClientNetManagerFactory clientNetManagerFactory2) {
-        assertDoesNotThrow(() -> clientNetManagerFactory1.create(this, "test_nick").joinGame(), "First join failed");
-        assertThrows(NickNotValidException.class, () -> clientNetManagerFactory2.create(this, "test_nick").joinGame(),
+    void twoPlayersTest(Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory1,
+                        Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory2) {
+        assertDoesNotThrow(() -> clientNetManagerFactory1.apply(this).createAndJoin("test_nick"), "First join failed");
+        assertThrows(NickNotValidException.class, () -> clientNetManagerFactory2.apply(this).createAndJoin("test_nick"),
                 "Same nick not failed");
     }
 
     @ParameterizedTest
     @MethodSource("twoPlayersTest")
-    void twoPlayersConcurrentTest(TestClientNetManagerFactory clientNetManagerFactory1,
-                                  TestClientNetManagerFactory clientNetManagerFactory2)
+    void twoPlayersConcurrentTest(Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory1,
+                                  Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory2)
             throws InterruptedException {
         List<Throwable> throwableList = new CopyOnWriteArrayList<>();
 
         Thread t1 = new Thread(() -> {
             try {
-                clientNetManagerFactory1.create(this, "test_nick").joinGame();
+                clientNetManagerFactory1.apply(this).createAndJoin("test_nick");
             } catch (Exception e) {
                 throwableList.add(e);
             }
@@ -115,7 +123,7 @@ public class JoinGameTest {
 
         Thread t2 = new Thread(() -> {
             try {
-                clientNetManagerFactory2.create(this, "test_nick").joinGame();
+                clientNetManagerFactory2.apply(this).createAndJoin("test_nick");
             } catch (Exception e) {
                 throwableList.add(e);
             }
@@ -147,26 +155,34 @@ public class JoinGameTest {
 
     @ParameterizedTest
     @MethodSource("threePlayersTest")
-    void threePlayersGameStartedTest(TestClientNetManagerFactory clientNetManagerFactory1,
-                                     TestClientNetManagerFactory clientNetManagerFactory2,
-                                     TestClientNetManagerFactory clientNetManagerFactory3)
+    void threePlayersGameStartedTest(Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory1,
+                                     Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory2,
+                                     Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory3)
             throws Exception {
         CompletableFuture<GameView> game1 = new CompletableFuture<>();
         CompletableFuture<GameView> game2 = new CompletableFuture<>();
         assertDoesNotThrow(() -> {
-            var player1 = clientNetManagerFactory1.create(this, "player1").joinGame();
+            var player1 = clientNetManagerFactory1.apply(this).createAndJoin("player1");
             player1.lobby().game().registerObserver(g -> {
-                assertNotNull(g);
-                game1.complete(g.game());
+                try {
+                    assertNotNull(g);
+                    game1.complete(g.game());
+                } catch (Throwable t) {
+                    game1.completeExceptionally(t);
+                }
             });
             player1.controller().ready(true);
             player1.controller().setRequiredPlayers(0);
         });
         assertDoesNotThrow(() -> {
-            var player2 = clientNetManagerFactory2.create(this, "player2").joinGame();
+            var player2 = clientNetManagerFactory2.apply(this).createAndJoin("player2");
             player2.lobby().game().registerObserver(g -> {
-                assertNotNull(g);
-                game2.complete(g.game());
+                try {
+                    assertNotNull(g);
+                    game2.complete(g.game());
+                } catch (Throwable t) {
+                    game2.completeExceptionally(t);
+                }
             });
             player2.controller().ready(true);
         });
@@ -174,28 +190,27 @@ public class JoinGameTest {
         game1.get(500, TimeUnit.MILLISECONDS);
         game2.get(500, TimeUnit.MILLISECONDS);
 
-        assertThrows(NickNotValidException.class, () -> clientNetManagerFactory3.create(this, "player2").joinGame(),
+        assertThrows(NickNotValidException.class, () -> clientNetManagerFactory3.apply(this).createAndJoin("player2"),
                 "Same nick not failed");
     }
 
     @ParameterizedTest
     @MethodSource("twoPlayersTest")
-    void twoPlayersGameStartedConcurrentReadyTest(TestClientNetManagerFactory clientNetManagerFactory1,
-                                                  TestClientNetManagerFactory clientNetManagerFactory2)
+    void twoPlayersGameStartedConcurrentReadyTest(Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory1,
+                                                  Function<JoinGameTest, TestClientNetManagerFactory> clientNetManagerFactory2)
             throws Exception {
         List<Throwable> throwableList = new CopyOnWriteArrayList<>();
-        AtomicReference<LobbyAndController<? extends LobbyView>> player1 = new AtomicReference<>();
-        AtomicReference<LobbyAndController<? extends LobbyView>> player2 = new AtomicReference<>();
-        player1.set(clientNetManagerFactory1.create(this, "player1").joinGame());
-        player2.set(clientNetManagerFactory2.create(this, "player2").joinGame());
+        var player1 = clientNetManagerFactory1.apply(this).createAndJoin("player1");
+        var player2 = clientNetManagerFactory2.apply(this).createAndJoin("player2");
+
         Thread t1 = new Thread(() -> {
             try {
                 Random r = new Random();
                 for (int i = 0; i < 20; i++) {
-                    Objects.requireNonNull(player1.get()).controller().ready(r.nextBoolean());
+                    player1.controller().ready(r.nextBoolean());
                     Thread.sleep(r.nextInt(10));
                 }
-                Objects.requireNonNull(player1.get()).controller().ready(true);
+                player1.controller().ready(true);
             } catch (Exception e) {
                 throwableList.add(e);
             }
@@ -205,10 +220,10 @@ public class JoinGameTest {
             try {
                 Random r = new Random();
                 for (int i = 0; i < 20; i++) {
-                    Objects.requireNonNull(player2.get()).controller().ready(r.nextBoolean());
+                    player2.controller().ready(r.nextBoolean());
                     Thread.sleep(r.nextInt(10));
                 }
-                Objects.requireNonNull(player2.get()).controller().ready(true);
+                player2.controller().ready(true);
             } catch (Exception e) {
                 throwableList.add(e);
             }
@@ -222,7 +237,12 @@ public class JoinGameTest {
         t2.join(500);
         assertFalse(t2.isAlive());
 
-        throwableList.forEach(Throwable::printStackTrace);
-        assertTrue(throwableList.isEmpty());
+        assertDoesNotThrow(() -> {
+            if (!throwableList.isEmpty()) {
+                var ex = new Exception("Test failed");
+                throwableList.forEach(ex::addSuppressed);
+                throw ex;
+            }
+        });
     }
 }
