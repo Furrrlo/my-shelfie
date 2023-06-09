8eaa647cf0662d11297c660f0cd70acad1c22a95
FrancescoGuarda <francesco1.guarda@mail.polimi.it>
2023-06-09 16:44:50 +0200

    replaced colors with personalGoal tile pictures in ColorTileComponent

--- PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/ColorTileComponent.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/ColorTileComponent.java
@@ -1,10 +1,15 @@
 package it.polimi.ingsw.client.javafx;
 
+import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.Tile;
 import org.jetbrains.annotations.Nullable;
 
 import javafx.application.ConditionalFeature;
 import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.shape.Rectangle;
@@ -12,17 +17,28 @@ import javafx.scene.shape.Rectangle;
 public class ColorTileComponent extends AnchorPane {
 
     public ColorTileComponent(@Nullable Tile tile) {
-        if (tile != null) {
-            switch (tile.getColor()) {
-                case PINK -> setBackground(Background.fill(javafx.scene.paint.Color.PINK));
-                case GREEN -> setBackground(Background.fill(javafx.scene.paint.Color.GREEN));
-                case YELLOW -> setBackground(Background.fill(javafx.scene.paint.Color.YELLOW));
-                case BLUE -> setBackground(Background.fill(javafx.scene.paint.Color.BLUE));
-                case WHITE -> setBackground(Background.fill(javafx.scene.paint.Color.WHITE));
-                case LIGHTBLUE -> setBackground(Background.fill(javafx.scene.paint.Color.LIGHTBLUE));
-            }
-        } else
+        this(tile == null ? null : tile.getColor());
+    }
+
+    public ColorTileComponent(@Nullable Color color) {
+        if (color != null) {
+            final PixelReader reader = new Image(
+                    FxResources.getResourceAsStream("assets/personal goal cards/Personal_Goals.png")).getPixelReader();
+            final Image croppedTilePic = switch (color) {
+                case PINK -> new WritableImage(reader, 110, 105, 198, 198);
+                case GREEN -> new WritableImage(reader, 1058, 334, 198, 198);
+                case YELLOW -> new WritableImage(reader, 347, 792, 198, 198);
+                case BLUE -> new WritableImage(reader, 586, 105, 198, 198);
+                case WHITE -> new WritableImage(reader, 822, 562, 198, 198);
+                case LIGHTBLUE -> new WritableImage(reader, 586, 1249, 198, 198);
+            };
+            final ImageView tilePic = new ImageView(croppedTilePic);
+            tilePic.fitWidthProperty().bind(widthProperty());
+            tilePic.fitHeightProperty().bind(heightProperty());
+            getChildren().add(tilePic);
+        } else {
             setBackground(Background.EMPTY);
+        }
 
         if (Platform.isSupported(ConditionalFeature.SHAPE_CLIP)) {
             clipProperty().bind(layoutBoundsProperty().map(bounds -> {
