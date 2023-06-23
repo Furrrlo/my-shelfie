b8146440ac9d1a1309c4cce589bb991a759ee950
Furrrlo <ferloo@protonmail.com>
2023-06-23 17:41:55 +0200

    Rewrite yet again SerializableProperty without broken comparator
    :cactus:

--- PSP031-common/src/main/java/it/polimi/ingsw/model/SerializableProperty.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/model/SerializableProperty.java
@@ -9,8 +9,10 @@ import java.io.ObjectStreamException;
 import java.io.Serial;
 import java.io.Serializable;
 import java.lang.ref.WeakReference;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -22,7 +24,8 @@ public class SerializableProperty<T> implements Property<T>, Serializable {
 
     private T val;
 
-    private final transient Set<Observer<? super T>> observers = new ConcurrentSkipListSet<>();
+    private final transient Map<ObserverBody<? super T>, Long> observersToInsertionTime = new ConcurrentHashMap<>();
+    private final transient Set<ObserverInsertion<? super T>> sortedObservers = new ConcurrentSkipListSet<>();
 
     @SuppressWarnings("NullAway") // NullAway sadly doesn't implement generics properly yet
     public static <T> SerializableProperty<@Nullable T> nullableProperty(@Nullable T prop) {
@@ -56,9 +59,9 @@ public class SerializableProperty<T> implements Property<T>, Serializable {
     public void set(T val) {
         this.val = val;
 
-        for (var iter = observers.iterator(); iter.hasNext();) {
+        for (var iter = sortedObservers.iterator(); iter.hasNext();) {
             var observer = iter.next();
-            var body = observer.body().get();
+            var body = observer.body().supplier().get();
             // Discard possibly null references caused by WeakReferences
             if (body == null) {
                 iter.remove();
@@ -75,69 +78,85 @@ public class SerializableProperty<T> implements Property<T>, Serializable {
 
     @VisibleForTesting
     Set<Consumer<? super T>> getObservers() {
-        return observers.stream()
-                .map(o -> o.body().get())
+        return observersToInsertionTime.keySet().stream()
+                .map(o -> o.supplier().get())
                 .filter(Objects::nonNull)
                 .collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
     public void registerObserver(Consumer<? super T> o) {
-        observers.add(new Observer<>(o, System.currentTimeMillis()));
+        long insertionTime = System.currentTimeMillis();
+        var body = new ObserverBody<>(o);
+        if (observersToInsertionTime.putIfAbsent(body, insertionTime) == null)
+            sortedObservers.add(new ObserverInsertion<>(body, insertionTime));
     }
 
     @Override
     public void registerWeakObserver(Consumer<? super T> o) {
-        observers.add(new Observer<>(new WeakReference<>(o), System.currentTimeMillis()));
+        long insertionTime = System.currentTimeMillis();
+        var body = new ObserverBody<>(new WeakReference<>(o));
+        if (observersToInsertionTime.putIfAbsent(body, insertionTime) == null)
+            sortedObservers.add(new ObserverInsertion<>(body, insertionTime));
     }
 
     @Override
     public void unregisterObserver(Consumer<? super T> o) {
-        // We don't care about the millis, removal happens by equals and hashCode which ignore those
-        var removed = observers.remove(new Observer<>(o, 0));
-        if (!removed) {
-            // Since we use 0 millis, the set implementation might not be able to remove it
-            // by using the standard methods as using compareTo returns bad results
-            // Fall back to iterating if the first failed
-            // This is basically the bad unlucky O(n) path that is sure to work
-            observers.removeIf(obs -> {
-                var body = obs.body().get();
-                // Since we are iterating everything anyway, also delete expired WeakReferences (body == null)
-                return body == null || body.equals(o);
-            });
-        }
+        var body = new ObserverBody<>(o);
+        Long insertionTime = observersToInsertionTime.remove(body);
+        if (insertionTime != null)
+            sortedObservers.remove(new ObserverInsertion<>(body, insertionTime));
     }
 
     @Override
     public String toString() {
         return "PropertyImpl{" +
                 "value=" + val +
-                ", observers=" + observers +
+                ", observersToInsertionTime=" + observersToInsertionTime +
+                ", sortedObservers=" + sortedObservers +
                 '}';
     }
 
     /**
-     * Observer container class which makes them comparable by insertion time
+     * @param supplier supplier which returns the actual lambda which will be called
+     * @param <T> type of the data this observer is observing
+     */
+    private record ObserverBody<T>(Supplier<@Nullable Consumer<T>> supplier) {
+
+        private ObserverBody(WeakReference<Consumer<T>> body) {
+            this(body::get);
+        }
+
+        private ObserverBody(Consumer<T> body) {
+            this(() -> body);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (!(o instanceof ObserverBody<?> observer))
+                return false;
+            return Objects.equals(supplier.get(), observer.supplier.get());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(supplier.get());
+        }
+    }
+
+    /**
+     * Observer container class which makes them sortable by insertion time
      *
      * @param body actual observer body which will be called
      * @param insertionMillis time this observer was inserted
      * @param <T> type of the data this observer is observing
      */
-    private record Observer<T>(Supplier<@Nullable Consumer<T>> body, long insertionMillis) implements Comparable<Observer<?>> {
-
-        private Observer(WeakReference<Consumer<T>> body, long insertionMillis) {
-            this(body::get, insertionMillis);
-        }
-
-        private Observer(Consumer<T> body, long insertionMillis) {
-            this(() -> body, insertionMillis);
-        }
-
+    private record ObserverInsertion<T>(ObserverBody<T> body,
+            long insertionMillis) implements Comparable<ObserverInsertion<?>> {
         @Override
-        public int compareTo(SerializableProperty.Observer<?> o) {
-            // Ensure that comparing the same body returns 0, to respect equals and hashcode
-            if (Objects.equals(body.get(), o.body.get()))
-                return 0;
+        public int compareTo(ObserverInsertion<?> o) {
             // Sort by insertion time
             var discriminant = Long.compare(insertionMillis, o.insertionMillis);
             if (discriminant != 0)
@@ -145,20 +164,6 @@ public class SerializableProperty<T> implements Property<T>, Serializable {
             // We have the same discriminant but the two objects are different, so we can't return 0
             // Differentiate them by using their ref pointer value
             return Integer.compare(System.identityHashCode(this), System.identityHashCode(o));
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (!(o instanceof Observer<?> observer))
-                return false;
-            return Objects.equals(body.get(), observer.body.get());
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(body.get());
         }
     }
 }
