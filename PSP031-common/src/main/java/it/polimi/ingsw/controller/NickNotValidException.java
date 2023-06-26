18bd5ab6354f64e78229dabf76515dfea1031faf
jackorse <giacomo.orsenigo@gmail.com>
2023-06-26 15:45:23 +0200

    Removed unused constructors in NickNotValidException

--- PSP031-common/src/main/java/it/polimi/ingsw/controller/NickNotValidException.java
+++ PSP031-common/src/main/java/it/polimi/ingsw/controller/NickNotValidException.java
@@ -10,19 +10,8 @@ import java.util.Objects;
  */
 public class NickNotValidException extends Exception {
 
-    public NickNotValidException() {
-    }
-
     public NickNotValidException(String message) {
         super(message);
-    }
-
-    public NickNotValidException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NickNotValidException(Throwable cause) {
-        super(cause);
     }
 
     @Override
