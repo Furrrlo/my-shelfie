ff34d47d9cea70d89696b367ea257f806c1aee35
FrancescoGuarda <francesco1.guarda@mail.polimi.it>
2023-06-27 09:26:13 +0200

    fixed DialogVbox to resize text according to window and QuitGameDialog

--- PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/DialogVbox.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/DialogVbox.java
@@ -15,46 +15,68 @@ import javafx.scene.text.TextAlignment;
  */
 abstract class DialogVbox extends VBox {
 
+    private final Label errorName;
+    private final Label dialogue;
+
     public DialogVbox(String errorTitle, Color bgColor, Color errorBgColor, String errorMessage) {
         setSpacing(15);
         backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
                 bgColor,
-                new CornerRadii(Math.min(10, 10 * (width.doubleValue() / 210d))),
-                new Insets(-10)))));
+                new CornerRadii(Math.min(10, 10 * (width.doubleValue() / 230d))),
+                new Insets(0)))));
 
-        Label errorName = new Label("Error: " + errorTitle);
+        this.errorName = new Label("Error: " + errorTitle);
         Fonts.changeWeight(errorName.fontProperty(), FontWeight.EXTRA_BOLD);
         errorName.backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
                 errorBgColor,
                 new CornerRadii(Math.min(5, 5 * (width.doubleValue() / 210d))),
-                new Insets(-5)))));
+                new Insets(0)))));
+
         errorName.setAlignment(Pos.CENTER);
-        errorName.prefWidthProperty().bind(this.widthProperty());
         this.getChildren().add(errorName);
 
-        Label dialogue = new Label(errorMessage);
+        this.dialogue = new Label(errorMessage);
         dialogue.setWrapText(true);
         dialogue.backgroundProperty().bind(widthProperty().map(width -> new Background(new BackgroundFill(
                 Color.WHITE,
                 new CornerRadii(Math.min(5, 5 * (width.doubleValue() / 210d))),
-                new Insets(-5)))));
-        dialogue.setAlignment(Pos.CENTER);
+                new Insets(0)))));
         dialogue.setTextAlignment(TextAlignment.CENTER);
-        dialogue.setGraphicTextGap(20);
         this.getChildren().add(dialogue);
-        dialogue.prefWidthProperty().bind(widthProperty());
-        dialogue.prefHeightProperty().bind(heightProperty());
+
+    }
+
+    @Override
+    protected void layoutChildren() {
+        super.layoutChildren();
+        double scale = Math.min(getWidth() / 230d, getHeight() / 230d);
+
+        final double border = 5 * scale;
+        double text_height = 14 * scale;
+        double header_height = 28 * scale;
+
+        //5 + 28 + 5 + messaggio + 5 + bottone + 28 + 5 = 230
+
+        this.errorName.resizeRelocate(border, border, getWidth() - 2 * border, header_height);
+        this.dialogue.resizeRelocate(border, text_height * 2 + 2 * border, getWidth() - 2 * border,
+                154 * scale);
+
+        dialogue.setGraphicTextGap(40);
+
+        Fonts.changeSize(errorName.fontProperty(), text_height);
+        Fonts.changeSize(dialogue.fontProperty(), text_height);
+
     }
 
     protected static class DialogButton extends InGameButton {
-
         public DialogButton(String text, @Nullable Color bgColor) {
             super(text, bgColor);
 
             backgroundRadiusProperty().bind(parentProperty()
                     .flatMap(p -> p instanceof Region r ? r.widthProperty() : null)
                     .map(w -> new CornerRadii(Math.min(5, 5 * (w.doubleValue() / 210d)))));
-            setBackgroundInsets(new Insets(-5));
+            setBackgroundInsets(new Insets(0));//anche qui erano 5
         }
+
     }
 }
--- PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/QuitGameDialog.java
+++ PSP031-client/src/main/java/it/polimi/ingsw/client/javafx/QuitGameDialog.java
@@ -1,14 +1,16 @@
 package it.polimi.ingsw.client.javafx;
 
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.FontWeight;
 
 /**
  * Message dialog asking for confirmation to exit the game
  */
 class QuitGameDialog extends DialogVbox {
+    private final Button cancel;
+    private final Button quitGame;
 
     public QuitGameDialog() {
         super("Quit Game",
@@ -16,20 +18,37 @@ class QuitGameDialog extends DialogVbox {
                 "Are you sure you want to quit the game?\nIf you quit the game you will have " +
                         "only 30 seconds to reconnect, otherwise the game will end and will not resume.");
 
-        HBox hbox = new HBox();
-        hbox.setSpacing(15);
-        hbox.prefWidthProperty().bind(widthProperty());
-        hbox.setAlignment(Pos.CENTER_RIGHT);
-
-        Button cancel = new DialogButton("Go back", Color.LIGHTSEAGREEN);
+        this.cancel = new DialogButton("Go back", Color.LIGHTSEAGREEN);
         cancel.setOnMouseClicked(e -> this.setVisible(false));
-        hbox.getChildren().add(cancel);
 
-        Button quitGame = new DialogButton("Quit Game", Color.INDIANRED);
+        this.quitGame = new DialogButton("Quit Game", Color.INDIANRED);
         // The stage has a onHidden handler which will handle the closing
         quitGame.setOnMouseClicked(event -> getScene().getWindow().hide());
 
-        hbox.getChildren().add(quitGame);
-        this.getChildren().add(hbox);
+        this.getChildren().add(quitGame);
+        this.getChildren().add(cancel);
+    }
+
+    @Override
+    protected void layoutChildren() {
+        super.layoutChildren();
+        double scale = Math.min(getWidth() / 230d, getHeight() / 230d);
+
+        double border = 5 * scale;
+        double width = getWidth() / 2 - 2 * border;
+        double text_height = 14 * scale;
+        double button_height = 28 * scale;
+
+        Fonts.changeSize(cancel.fontProperty(), text_height);
+        Fonts.changeSize(quitGame.fontProperty(), text_height);
+        Fonts.changeWeight(quitGame.fontProperty(), FontWeight.BOLD);
+        Fonts.changeWeight(cancel.fontProperty(), FontWeight.BOLD);
+
+        this.setBorder(new Border(new BorderStroke(Color.INDIANRED,
+                BorderStrokeStyle.SOLID, new CornerRadii(Math.min(10, 10 * (getWidth() / 230d))), BorderWidths.DEFAULT)));
+
+        this.cancel.resizeRelocate(border, getHeight() - button_height - border, width, button_height);
+        this.quitGame.resizeRelocate(3 * border + width, getHeight() - button_height - border, getWidth() / 2 - 2 * border,
+                button_height);
     }
 }
